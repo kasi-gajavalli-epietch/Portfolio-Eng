6e0474de7aca4bc82bb2ffe34b0d596f70d95c69
Kasi GAJAVALLI <kasi.gajavalli@gmail.com>
2022-12-07 15:27:55 +0100

    Portfolio_eng

---
+++ my-app/src/components/Resume/Data.jsx
@@ -0,0 +1,41 @@
+const Data = [
+
+    {
+      id: 1,
+      category: "experience",
+      icon: "icon-briefcase",
+      year: "10/2021 - 05/2022",
+      title: "Jr. Web Developper- Private Consultant",
+      place: "Digitalphy, Benagaluru , India.",
+      desc: "Develop websites, explore technologies for enterprises and understand the bussiness.",
+    },
+    {
+      id: 2,
+      category: "experience",
+      icon: "icon-briefcase",
+      year: "01/2020 - 03/2021",
+      title: "Research Engineer - Developper",
+      place: "CEA Cadarache, France.",
+      desc: "Develop statistical models, integrate codes using tools, data analysis with python, data modeling with OpenCalphad.",
+    },
+    {
+      id: 3,
+      category: "experience",
+      icon: "icon-briefcase",
+      year: "2009 - 2013",
+      title: "Research Engineer-Incharge of Project Affairs ",
+      place: "CEA Cadarache, France.",
+      desc: "Develop new experimental protocols, measure and refine data from experiments and literature, analyze and model data with python, excel and Calphad.",
+    },
+    {
+      id: 4,
+      category: "experience",
+      icon: "icon-briefcase",
+      year: "2009 - 2013",
+      title: "M2 Internship-Developper ",
+      place: "Areva-CEA Marcoule, France.",
+      desc: "FEM Code development and benchmarking, data modeling to simulate the effects of stress using molecular dynamics.",
+    },
+  ];
+
+export default Data
---
+++ my-app/src/components/Resume/Resume.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import "./Resume.css";
+import Data from "./Data";
+import Card from "./Card";
+
+const Resume = () => {
+  return (
+    <section className="resume container section" id="experience">
+        <h2 className="section__title">Experience</h2>
+
+        <div className="resume__container grid">
+          
+          <div className="timeline grid">
+            {/*eslint-disable-next-line*/}
+            {Data.map((val, index) => {
+                if (val.category === "experience"){
+                    return (
+                        <Card key={index} 
+                            icon={val.icon} 
+                            title={val.title} 
+                            place={val.place}
+                            year={val.year} 
+                            desc={val.desc} />
+                    )
+                }
+            })}
+          </div>
+        </div>
+    </section>
+  )
+}
+
+export default Resume
